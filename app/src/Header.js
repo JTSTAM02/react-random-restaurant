b075554ce104b1671f6b28a73d755f0baf16e45c
Tyler Stamm <tyler.stamm1313@gmail.com>
2023-06-29 14:39:22 +0000

    Updates

---
+++ app/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Header() {
+    return (
+        <>
+            <h1>GameDay's by Awesome Inc.</h1>
+            <h2> Address: 348 E Main Street<br />
+            Lexington, KY</h2>
+            <h3>Hours: 7 AM - 1 PM</h3>
+        
+        
+        </>
+    )
+}
---
+++ app/src/Menu.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+export default function Menu() {
+    const [menuItems, setMenuItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get('https://www.jsonkeeper.com/b/MDXW')
+            .then(response => {
+                setMenuItems(response.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching menu items', error);
+                setLoading(false);
+            });
+    }, []);
+
+    if(loading) {
+        return(
+            <div>Loading...</div>
+        )
+    }
+    return (
+        <>
+            <h2>Menu</h2>
+            <ul>
+                menuItems.map ( item => (
+                    <li key= {menuItems.id}>{menuItems.name}</li>
+                ))}
+            </ul>
+        
+        </>
+    )
+}
